b2a11e74448109d577c79a048f4a9e5dccc086f4
milky <milky@Milky_sin_pc.lan>
2023-09-02 15:52:28 +0200

    HAHAHA!! Ferdig

--- Oppgave 1 - Lambda-uttrykk/src/b/Oppgave1b.java
+++ Oppgave 1 - Lambda-uttrykk/src/b/Oppgave1b.java
@@ -13,42 +13,39 @@ public class Oppgave1b {
 		
 		return svar;
 	}
-	
 	public static int beregn (int a, int b, int summerFunksjon) {
 		
-		int sum = a+b;
-		int størst =0;
-		int Avstand = a-b;
-		
-		
-		if (summerFunksjon>5) {
-			 System.out.println("Summen av " + a 
-					+ " og " + b + " = " 
-					+ sum);
-		}
-		else if (summerFunksjon>0 || summerFunksjon<=5) {
-			if (a>b) {
-				a = størst;
-			}
-			else if (a<b) {
-				b = størst;
-			}
-			else {
-				System.out.println("a og b er like store");
-			}
-		}
-		else if (summerFunksjon<0) {
-			System.out.println("Avstanden (absoluttverdien av differansen) "
-					+ "mellom " + a +
-					" og " + b + " = " + 
-					Avstand);
-		}
-		
-		return 0;
+		int resultat = 0;
+
+        switch (summerFunksjon) {
+            case 1: // Summen
+                resultat = a + b;
+                break;
+            case 2: // Største av de to tallene
+                resultat = Math.max(a, b);
+                break;
+            case 3: // Avstanden (absoluttverdien av differansen)
+                resultat = Math.abs(a - b);
+                break;
+            default:
+                System.out.println("Ugyldig valg.");
+        }
+
+        return resultat;
 		
 	}
 	public static void main(String[] args) {
+		int sum = beregn(12,13,1); //Summen av 12 og 13
+		System.out.println("Summen av 12 og 13 er: " + sum);
 		
+		
+		int maks = beregn(-5,3,2); //Den største av -5 og 3
+		System.out.println("Det største av -5 og 3 er: " + maks);
+		
+		int avstand = beregn(54,45,3); /*Avstanden (absoluttverdien
+										av differansen) 
+										mellom 54 og 45*/
+		System.out.println("Avstanden mellom 54 og 45 er: " + avstand);
 	}
 
 }
