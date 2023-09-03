523bb5cafa7059a1e7c0d6d50270c5126ad49a14
milky <milky@Milky_sin_pc.lan>
2023-09-03 18:54:58 +0200

    For the Republic!!!!

---
+++ Oppgave 2 - Lambda-uttrykk/src/demo/Ansatt.java
@@ -0,0 +1,44 @@
+package demo;
+
+import  demo.Kjonn;
+import java.util.*;
+public class Ansatt {
+
+	private String fornavn;
+	private String etternavn;
+	private Kjonn kjonn;
+	private String stilling;
+	private int aarslønn;
+	
+	public Ansatt (String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslønn) {
+		this.fornavn=fornavn;
+		this.etternavn=etternavn;
+		this.kjonn=kjonn;
+		this.stilling=stilling;
+		this.aarslønn=aarslønn;
+	}
+	
+	public String getFornavn() {
+		return fornavn;
+	}
+	
+	public String getEtternavn() {
+		return etternavn;
+	}
+	
+	public Kjonn getKjonn() {
+		return kjonn;
+	}
+	
+	public String getStilling() {
+		return stilling;
+	}
+	
+	public int getAarslønn() {
+		return aarslønn;
+	}
+	
+	public void setaarslonn(int aarslonn) {
+        this.aarslønn = aarslonn;
+    }
+}
---
+++ Oppgave 2 - Lambda-uttrykk/src/demo/Oppg2.java
@@ -0,0 +1,66 @@
+package demo;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class Oppg2 {
+
+	private static void lonnsoppgjor(List<Ansatt> ansatte, Function<Ansatt, Integer> lonnsendring) {
+        for (Ansatt ansatt : ansatte) {
+            int nyLonnsendring = lonnsendring.apply(ansatt);
+            ansatt.setaarslonn(nyLonnsendring);
+        }
+    }
+	
+	private static void skrivUtAlle(List<Ansatt> ansatte) {
+		for (Ansatt ansatt: ansatte) {
+			System.out.println("Fornavn: " + ansatt.getFornavn() 
+			+ " Etternavn: " + ansatt.getEtternavn()
+			+ "\nKjønn: " + ansatt.getKjonn()
+			+"\nStilling: " + ansatt.getStilling()
+			+ "\nÅrslønn: " + ansatt.getAarslønn()+" Imperial Credits"
+			+ "\n");
+		}
+	}
+	
+	public static void main(String[] args) {
+		//Lag en Ansatt type liste som heter ansatte
+		List<Ansatt> ansatte = new ArrayList<>();
+		/*new Ansatt er 
+		 * Fornavn,etternavn,kjønn,stilling,lønn
+		 * */
+		ansatte.add(new Ansatt("Aayla ", "Secura ", Kjonn.Dame, "Soldat", 350000));
+		ansatte.add(new Ansatt("Anakin ", "Skywalker ", Kjonn.Mann, "General",10000000));
+		ansatte.add(new Ansatt("Obi-Wan", " Kenobi", Kjonn.Mann, "General",10000000));
+		ansatte.add(new Ansatt("Ahsoka "," Tano ", Kjonn.Dame, "Ex-General",10000000));
+		ansatte.add(new Ansatt("Darth ", " Vader", Kjonn.Mann, "Leiemorder", 12345443));
+		
+		//i. Et fast kronetillegg
+		lonnsoppgjor(ansatte, ansatt -> ansatt.getAarslønn()+1500);
+		
+		//ii. Et fast prosenttillegg (+15% i dette tilfelle)
+		lonnsoppgjor(ansatte,ansatt->ansatt.getAarslønn()+(int)(ansatt.getAarslønn()*0.15));
+	
+		//iii. Et fast kronetillegg hvis du har lav lønn
+		lonnsoppgjor(ansatte, ansatt -> {
+            if (ansatt.getAarslønn() < 30000) {
+                return ansatt.getAarslønn() + 2000; // Fast kronetillegg hvis lav lønn
+            } else {
+                return ansatt.getAarslønn();
+            }
+	});
+		
+		//iv. Et fast prosenttillegg hvis du er mann
+		lonnsoppgjor(ansatte, ansatt -> {
+            if (ansatt.getKjonn() == Kjonn.Mann) {
+                return ansatt.getAarslønn() + (int)(ansatt.getAarslønn() * 0.03); // Fast prosenttillegg for menn
+            } else {
+                return ansatt.getAarslønn();
+            }
+        });
+		
+		// Skriver ut hele listen av ansatte
+		skrivUtAlle(ansatte);
+
+}
+	}
